8c26e683ec39b7e634523f44e6573862f279013c
Anthony Grullon <agrullon95@gmail.com>
2016-07-11 13:29:44 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Parallax
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+// Google maps
+
+// function init_map() {
+//             var var_location = new google.maps.LatLng(41.291044, -73.923684);
+//
+//             var var_mapoptions = {
+//                 center: var_location,
+//                 zoom: 16
+//             };
+//
+//             var var_marker = new google.maps.Marker({
+//                 position: var_location,
+//                 map: var_map,
+//                 title: "Video Conferencia Intl"
+//             });
+//
+//             var var_map = new google.maps.Map(document.getElementById("first-map"),
+//                 var_mapoptions);
+//
+//             var_marker.setMap(var_map);
+//
+//         }
+//
+//  google.maps.event.addDomListener(window, 'load', init_map);
